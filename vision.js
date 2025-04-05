7a82b61db03368d9a654d5eb60392667e3214f42
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2025-04-05 13:23:47 -0700

    get all body parts

--- vision.js
+++ vision.js
@@ -3,6 +3,7 @@ let video;
 let bodyPose;
 let connections;
 let poses = [];
+let dotSize = 15;
 
 function preload() {
   bodyPose = ml5.bodyPose("MoveNet", { flipped: true });
@@ -13,48 +14,43 @@ function mousePressed() {
 }
 
 function gotPoses(results) {
-  // Store detected poses in the global array
   poses = results;
+  //code starts here
+
 }
 
 function setup() {
-  // Create canvas for displaying video feed
   createCanvas(640, 480);
-
-  // Capture live video with flipped orientation
   video = createCapture(VIDEO, { flipped: true });
   video.hide();
-
-  // Start detecting poses from the video feed
   bodyPose.detectStart(video, gotPoses);
 }
 
 function draw() {
-  // Display the live video feed
   image(video, 0, 0);
-
-  // Ensure at least one pose is detected before proceeding
   if (poses.length > 0) {
     let pose = poses[0];
-
-    // Draw a circle at the detected nose position
-    fill(236, 1, 90);
+    fill(255,0,0);
     noStroke();
-    circle(pose.nose.x, pose.nose.y, 48);
+    ellipse(pose.nose.x, pose.nose.y, dotSize)
+    ellipse((pose.right_hip.x + pose.left_hip.x)/2, (pose.right_hip.y + pose.left_hip.y)/2, dotSize)
 
-    // Draw circles at the detected shoulder positions
-    fill(146, 83, 161);
-    if (pose.right_knee.confidence >0.1){
-      circle(pose.right_knee.x, pose.right_knee.y, 64);
-    }
-    if (pose.left_knee.confidence >0.1){
-        circle(pose.left_knee.x, pose.left_knee.y, 64);
-      }
-    if (pose.left_shoulder.confidence >0.1){
-      circle(pose.left_shoulder.x, pose.left_shoulder.y, 64);
-    }
-    if (pose.right_shoulder.confidence >0.1){
-        circle(pose.right_shoulder.x, pose.right_shoulder.y, 64);
-    }
+    ellipse(pose.right_wrist.x, pose.right_wrist.y, dotSize)
+    ellipse(pose.left_wrist.x, pose.left_wrist.y, dotSize)
+
+    ellipse(pose.right_elbow.x, pose.right_elbow.y, dotSize)
+    ellipse(pose.left_elbow.x, pose.left_elbow.y, dotSize)
+    
+    ellipse(pose.right_shoulder.x, pose.right_shoulder.y, dotSize)
+    ellipse(pose.left_shoulder.x, pose.left_shoulder.y, dotSize)
+    
+    ellipse(pose.right_knee.x, pose.right_knee.y, dotSize)
+    ellipse(pose.left_knee.x, pose.left_knee.y, dotSize)
+
+    ellipse(pose.right_ankle.x, pose.right_ankle.y, dotSize)
+    ellipse(pose.left_ankle.x, pose.left_ankle.y, dotSize)
+
+    ellipse(pose.right_hip.x, pose.right_hip.y, dotSize)
+    ellipse(pose.left_hip.x, pose.left_hip.y, dotSize)
   }
 }
