e6b3dec050b8d5817b7444e56d3a2ea6559c64d5
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2025-04-06 04:13:40 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function myFunction(x) {
+    if (x.matches) { 
+        //phone
+        document.getElementById("cp").style.display = "none"
+        document.getElementById("c").style.display = "none"
+        document.getElementById("pc").style.display = "block"
+        document.getElementById("p").style.display = "block"
+    } else {
+        //computer
+        document.getElementById("pc").style.display = "none"
+        document.getElementById("p").style.display = "none"
+        document.getElementById("cp").style.display = "inline"
+        document.getElementById("c").style.display = "inline"
+    }
+  }
+  
+  // Create a MediaQueryList object
+  var x = window.matchMedia("(max-width: 700px)")
+  
+  // Call listener function at run time
+  myFunction(x);
+  
+  // Attach listener function on state changes
+  x.addEventListener("change", function() {
+    myFunction(x);
+  });
