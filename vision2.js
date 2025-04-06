b8292e68db67149ffb2b6143bfb555aab377117b
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2025-04-06 06:01:13 -0700

    Update vision2.js

--- vision2.js
+++ vision2.js
@@ -217,7 +217,8 @@ function gotPoses(results) {
 }
 
 function setup() {
-  createCanvas(640, 480);
+  var canvas = createCanvas(640, 480);
+  canvas.parent('workoutTop')
   video = createCapture(VIDEO, { flipped: true });
   video.hide();
   bodyPose.detectStart(video, gotPoses);
